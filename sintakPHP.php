7072f41fa750a7980ffa226e4824582b39934552
justadhy <adhykartansa@email.unikom.ac.id>
2015-03-07 13:46:59 +0700

    Push file PHP for the first time

---
+++ sintakPHP.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>Sintaks PHP Kelompok 2</title>
+</head>
+<body>
+
+	<?php
+		echo"<h1 align='center'>Kelompok 2</h1>";
+		echo"<br><br><center>";
+		echo"10112268 - Adhy Kartansa";
+	?>
+
+</body>
+</html>
