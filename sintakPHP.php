58914939dbb6d5f86c41b4698d5fc569057ddca0
teddykhairul <teddykhairul@email.unikom.ac.id>
2015-03-09 20:46:27 +0700

    Update perbaikan

--- sintakPHP.php
+++ sintakPHP.php
@@ -8,12 +8,8 @@
 		echo"<h1 align='center'>Kelompok 2</h1>";
 		echo"<br><br><center>";
 		echo"10112268 - Adhy Kartansa";
-<<<<<<< HEAD
 		echo"10112267 - Jajang Sumardi";
-=======
-		
 		echo"10112282 - Teddy Khairul";
->>>>>>> b5374b719c09455afc5e4054ddf0ed0ad5cea3d5
 	?>
 
 </body>
