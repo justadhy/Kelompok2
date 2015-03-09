35bd9d310e178872b7d37a65f17cb6ce1d3d76f2
ranysella <ranysella@gmail.com>
2015-03-10 00:20:57 +0700

    Penambahan nama edit garis

--- sintakPHP.php
+++ sintakPHP.php
@@ -6,10 +6,16 @@
 
 	<?php
 		echo"<h1 align='center'>Kelompok 2</h1>";
-		echo"<br><br><center>";
+		echo"<hr color='#FF0000'>";
+		echo"<br><center>";
 		echo"10112268 - Adhy Kartansa";
+		echo"<br />";
 		echo"10112267 - Jajang Sumardi";
+		echo"<br />";
 		echo"10112282 - Teddy Khairul";
+		echo"<br />";
+		echo"10112289 - Ranny Rosella";
+		echo"<br />";
 		echo"test 1";
 	?>
 
